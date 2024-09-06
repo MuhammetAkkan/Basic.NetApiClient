ea55cc5d464f3a0ec16a458b24051e824bce9c7f
muhammetakkan <muhammettakkann@gmail.com>
2024-09-06 17:01:07 +0300

    first commit

---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public async Task<IActionResult> Index()
+        {
+            var products = new List<ProductDTO>();
+
+            using (HttpClient httpClient = new())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7033/api/Product/GetProducts"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var apiResponse = await response.Content.ReadAsStringAsync();
+
+                        // JSON verisini deserialize ederken hata yakalama
+                        try
+                        {
+                            products = JsonSerializer.Deserialize<List<ProductDTO>>(apiResponse) ?? new List<ProductDTO>();
+                        }
+                        catch (JsonException ex)
+                        {
+                            // JSON parse hatası
+                            return BadRequest($"JSON parse hatası: {ex.Message}");
+                        }
+                    }
+                    else
+                    {
+                        return BadRequest("API çağrısı başarısız oldu.");
+                    }
+                }
+            }
+
+            return View(products);
+        }
+
+
+
+
+    }
+}
---
+++ WebUI/Models/ProductDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebUI.Models
+{
+    public class ProductDTO
+    {
+        //Api projesindeki ProductDTO as
+
+        [JsonPropertyName("productId")]
+        public int ProductId { get; set; }
+        [JsonPropertyName("productName")]
+        public string ProductName { get; set; } = null!;
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+    }
+}
